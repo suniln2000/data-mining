e2157aad274ddafa2257e569efaf729abed7e6b8
Sunil N <sunilmon2000@gmail.com>
2024-02-05 11:41:29 +0530

    defines the shoes pages and iterate through a range of page

--- shoes/spiders/shoes.py
+++ shoes/spiders/shoes.py
@@ -2,3 +2,8 @@ import scrapy
 
 class ShoesSpider(scrapy.Spider):
     name = 'shoes'
+
+    def start_requests(self):
+        base_url = 'https://www.mytheresa.com/int/en/men/shoes?page={}'
+        start_page = 1
+        end_page = 3
