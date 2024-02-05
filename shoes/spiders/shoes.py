0ac0d391ff8fa24a84f103baad34a588065197a7
Sunil N <sunilmon2000@gmail.com>
2024-02-05 11:43:17 +0530

    For each page, a request is created

--- shoes/spiders/shoes.py
+++ shoes/spiders/shoes.py
@@ -7,3 +7,7 @@ class ShoesSpider(scrapy.Spider):
         base_url = 'https://www.mytheresa.com/int/en/men/shoes?page={}'
         start_page = 1
         end_page = 3
+
+        for page in range(start_page, end_page + 1):
+            url = base_url.format(page)
+            yield scrapy.Request(url, callback=self.parse)
